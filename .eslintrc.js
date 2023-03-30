34d04442628086078ca5c5d531c58bc8fa3758f4
Christopher Nascone <chris@nasc.one>
2023-03-30 13:51:02 -0400

    feat: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/jest-testing-library",
+    "@remix-run/eslint-config/node",
+  ],
 };
