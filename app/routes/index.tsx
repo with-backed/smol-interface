c87822bf46f9a7a3db93cccb0927161aa9ee2b23
Christopher Nascone <chris@nasc.one>
2023-03-31 13:47:51 -0400

    wip: click-through slideshow

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
+import { useCallback, useState } from "react";
 import { Footer } from "~/components/Footer";
 import { Header } from "~/components/Header";
 import stylesUrl from "~/styles/index.css";
@@ -7,11 +8,28 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+const SLIDE_COUNT = 6;
+const SLIDES = new Array(SLIDE_COUNT)
+  .fill(undefined)
+  .map((_, i) => `/slides/${i}.png`);
+
 export default function Index() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const advanceSlide = useCallback(
+    () => setCurrentSlide((prev) => ++prev % SLIDE_COUNT),
+    []
+  );
+
   return (
     <div className="wrapper">
       <Header />
-      <div className="flex h-full"></div>
+      <div
+        className="content flex h-full items-center justify-center"
+        onClick={advanceSlide}
+      >
+        <img src={SLIDES[currentSlide]} alt="" />
+      </div>
       <Footer />
     </div>
   );
