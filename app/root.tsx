3d03431906a1ace62311008ef80a77d14a687160
Christopher Nascone <chris@nasc.one>
2023-04-04 14:20:37 -0400

    feat: Courier Prime

--- app/root.tsx
+++ app/root.tsx
@@ -16,7 +16,18 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400&display=swap",
+  },
+];
 
 export default function App() {
   return (
@@ -25,7 +36,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="flex justify-center items-center font-mono bg-no-repeat bg-cover">
+      <body className="flex justify-center items-center bg-no-repeat bg-cover">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
