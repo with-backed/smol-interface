4dd8d02b064caf614fa457bfc4f7d00b11734044
Christopher Nascone <chris@nasc.one>
2023-04-12 12:39:27 -0400

    feat: back, forward icons

---
+++ app/components/Back/Back.tsx
@@ -0,0 +1,11 @@
+export const Back = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={72} height={60} fill="none">
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="M38.07 21 29 30.07l9.07 9.072"
+    />
+  </svg>
+);
---
+++ app/components/Forward/Forward.tsx
@@ -0,0 +1,11 @@
+export const Forward = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={72} height={60} fill="none">
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={3}
+      d="m35.072 39.142 9.07-9.071L35.073 21"
+    />
+  </svg>
+);
