0be2e86a905911a0a69f06f07235f421be61d6de
Christopher Nascone <chris@nasc.one>
2023-04-12 12:39:48 -0400

    feat: footer has working nav

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,9 +1,64 @@
+import { Link, useLocation } from "@remix-run/react";
+import { useMemo } from "react";
+import { Back } from "~/components/Back";
+import { Forward } from "~/components/Forward";
+
+const PAGES = ["/", "/intro"];
+
+interface PageIndex {
+  index: number;
+  nextPath: string | undefined;
+  prevPath: string | undefined;
+}
+
+function useCurrentPageIndex(): PageIndex {
+  const { pathname } = useLocation();
+
+  const value = useMemo(() => {
+    const index = PAGES.indexOf(pathname);
+    const nextPath = PAGES[index + 1];
+    const prevPath = PAGES[index - 1];
+
+    return {
+      index,
+      nextPath,
+      prevPath,
+    };
+  }, [pathname]);
+
+  return value;
+}
+
+type ArrowLinkProps = {
+  to?: string;
+  direction: "forward" | "back";
+};
+
+function ArrowLink({ direction, to }: ArrowLinkProps) {
+  const Arrow = direction === "forward" ? Forward : Back;
+  if (!to) {
+    return (
+      <div className="opacity-25 cursor-not-allowed">
+        <Arrow />
+      </div>
+    );
+  }
+  return (
+    <Link to={to}>
+      <Arrow />
+    </Link>
+  );
+}
+
 export function Footer() {
+  const { index, nextPath, prevPath } = useCurrentPageIndex();
   return (
     <footer className="flex justify-between items-center bg-black text-white">
-      <span>&lt;</span>
-      <span>2 of 6</span>
-      <span>&gt;</span>
+      <ArrowLink to={prevPath} direction="back" />
+      <span>
+        {index + 1} of {PAGES.length}
+      </span>
+      <ArrowLink to={nextPath} direction="forward" />
     </footer>
   );
 }
