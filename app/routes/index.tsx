43e3227f7bddb9cc745e525efc414b18d80a94fe
Christopher Nascone <chris@nasc.one>
2023-04-27 15:59:35 -0400

    fix: index is better on mobile

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,8 @@
 export default function Index() {
   return (
-    <div className="graph-papr h-full w-full flex flex-col items-center">
-      <div className="my-14">
-        <img src="/title.svg" alt="Title" />
-      </div>
-      <div className="w-4/5 overflow-hidden rounded-full bg-white mb-16">
+    <div className="graph-papr h-full w-full flex flex-col items-center justify-center gap-14">
+      <img src="/title.svg" alt="Title" />
+      <div className="w-4/5 overflow-hidden rounded-full bg-white">
         <img src="/toad-1-thrillin.svg" alt="Toad" className="p-4" />
       </div>
       <div className="flex flex-col items-center">
