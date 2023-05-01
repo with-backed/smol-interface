21ced442e4447d604d210c7e575a6ed30b129f64
Christopher Nascone <chris@nasc.one>
2023-05-01 16:11:22 -0400

    fix: index better at various sizes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,8 @@
 export default function Index() {
   return (
-    <div className="graph-papr h-full w-full flex flex-col items-center">
-      <div className="my-14">
-        <img src="/title.svg" alt="Title" />
-      </div>
-      <div className="w-4/5 overflow-hidden rounded-full bg-white mb-16">
-        <img src="/toad-1-thrillin.svg" alt="Toad" className="p-4" />
-      </div>
+    <div className="graph-papr h-full w-full flex flex-col items-center justify-center gap-6">
+      <img src="/title.svg" alt="Title" />
+      <img src="/1-super-dance.svg" alt="Toad" />
       <div className="flex flex-col items-center">
         <p className="w-44 text-center">Send ur meme on a thrillin adventure</p>
         <img className="absolute" src="/whiskers.svg" alt="Whiskers" />
