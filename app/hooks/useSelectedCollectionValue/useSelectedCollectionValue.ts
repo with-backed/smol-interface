5198bdacf2cfa58a8be55a1d2533a471bd64613b
Christopher Nascone <chris@nasc.one>
2023-05-04 12:44:32 -0400

    fix: better token id

--- app/hooks/useSelectedCollectionValue/useSelectedCollectionValue.ts
+++ app/hooks/useSelectedCollectionValue/useSelectedCollectionValue.ts
@@ -4,9 +4,7 @@ import { useCollectionTwapBidChange } from "../useCollectionTwapBidChange";
 export function useSelectedCollectionValue() {
   const { collateralCount, collection } = useGlobalStore((s) => ({
     collection:
-      // it's really weird that this is the way we recover the collection address
-      s.selectedVault?.collateral[0].id.split("-")[0] ||
-      s.inProgressLoan?.collectionAddress,
+      s.selectedVault?.token.id || s.inProgressLoan?.collectionAddress,
     collateralCount:
       s.selectedVault?.collateral.length || s.inProgressLoan?.tokenIds.length,
   }));
